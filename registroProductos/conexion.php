8a95de283720471121e402fe635f7a2048be95e9
xMr-Blue <165853586+xMr-Blue@users.noreply.github.com>
2024-07-29 19:27:28 -0300

    Update conexion.php

--- registroProductos/conexion.php
+++ registroProductos/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
 
-$conexion = new mysqli("localhost","root","","login-test","3307");
+$conexion = new mysqli("localhost","root","","los4hermanos","3307");
 $conexion -> set_charset("utf8");
 ?>
